17a6b1e87439c79e43928df92ec8c4a5cf47069b
Saverio <saverio3107@gmail.com>
2023-08-07 12:00:04 +0200

    UPDATE

---
+++ src/App.js
@@ -0,0 +1,56 @@
+
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Post from './Post';
+
+const BASE_URL = 'http://localhost:8000/'
+
+function App() {
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch(BASE_URL + 'post/all')
+      .then(response => {
+        const json = response.json();
+        console.log(json);
+        if (response.ok) {
+          return json
+        }
+        throw response
+      })
+      .then(data => {
+        const result = data.sort((a, b) => {
+          const t_a = a.timestamp.split(/[-T:]/);
+
+          const t_b = b.timestamp.split(/[-T:]/);
+          const d_a = new Date(Date.UTC(t_a[0], t_a[1] - 1, t_a[2], t_a[3], t_a[4], t_a[5]));
+          const d_b = new Date(Date.UTC(t_b[0], t_b[1] - 1, t_b[2], t_b[3], t_b[4], t_b[5]));
+          return d_b - d_a
+
+        })
+        return result
+      })
+      .then(data => {
+        setPosts(data)
+      })
+      .catch(error => {
+        console.log(error);
+        alert(error)
+      });
+  }, [])
+
+  return (
+    <div className="app_posts">
+      {
+        posts.map(post => (
+          <Post post={post} />
+        ))
+      }
+
+    </div>
+
+  );
+}
+
+export default App;
